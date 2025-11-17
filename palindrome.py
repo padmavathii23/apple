7e62dfcc00c0fa636b47a78a81eccbfd19a6a21d
Padmavathi <padmavathi23prakash@gmail.com>
2025-11-17 14:23:26 +0530

    added new file

---
+++ palindrome.py
@@ -0,0 +1,10 @@
+# check if a list contains a palindrome of elements
+
+list = [23, 45, 66, 44, 23]
+copy = list.copy()
+copy.reverse()
+
+if (copy == str):
+    print("it is a palindrome")
+else:
+    print("not a palindrome")
